38b4e5517cc9cf945db0d26454351d3e426879a2
Matthias Frei <mf@frei.media>
2022-07-22 09:17:51 +0200

    cast ports in settings to int

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ from os import getenv
 
 
 class Settings:
-    port_http = getenv("PORT_HTTP", 8000)
-    port_https = getenv("PORT_HTTPS", 8001)
-    port_https_quic = getenv("PORT_HTTPS_QUIC", 8002)
+    port_http = int(getenv("PORT_HTTP", 8000))
+    port_https = int(getenv("PORT_HTTPS", 8001))
+    port_https_quic = int(getenv("PORT_HTTPS_QUIC", 8002))
 
     certfile = getenv("CERTFILE", "certs/server.crt")
     keyfile = getenv("KEYFILE", "certs/server.key")
